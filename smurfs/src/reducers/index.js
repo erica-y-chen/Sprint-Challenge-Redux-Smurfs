3fafe32900af1cde4de86dc9beda385b9ee475ab
erica-y-chen <eyufanchen@gmail.com>
2019-04-12 08:39:45 -0700

    completed reducers

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,23 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null,
+};
+
+export const smurfReducer = (state = initialState, action ) => {
+  console.log('reducer', action)
+  switch (action.type){
+    case FETCHING: 
+      return{
+        ...state, 
+      }
+  }
+
+}
